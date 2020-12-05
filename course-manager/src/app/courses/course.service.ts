4ff0d6751ddaf3190ef47860374b8744c36e8346
Marcilio <marciliorsilva93@gmail.com>
2020-12-05 18:44:43 -0300

    implementando delete course

--- course-manager/src/app/courses/course.service.ts
+++ course-manager/src/app/courses/course.service.ts
@@ -31,6 +31,10 @@ export class CourseService{
             return this.httpClient.post<Course>(`${this.coursesUrl}`, course);
         }
     }
+
+    deleteById(id:number): Observable<any>{
+        return this.httpClient.delete<any>(`${this.coursesUrl}/${id}`);
+    }
 }
 
 
